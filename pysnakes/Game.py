1d0566d392e28e3ab91c17ebb77e66d5167803e2
lucascprazeres <olucascostadp@gmail.com>
2020-05-25 10:43:41 -0300

    added snake movement

--- pysnakes/Game.py
+++ pysnakes/Game.py
@@ -6,24 +6,52 @@ class Game:
     def __init__(self):
         self.game_over = False
         self.window = None
+        self.clock = pygame.time.Clock()
         self.snake = None
+        # Game Rules
         self.snake_initial_pos = (200, 150)
+        self.snake_vel = 5
 
     def start(self):
         self.window = Window.Window((400, 300))
         self.snake = Snake(Window.COLORS["green"], self.snake_initial_pos)
 
         while not self.game_over:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.window.close()
+            self.listen_to_events()
+            self.snake.move()
+            self.refreshScreen()
             self.drawSnake()
             pygame.display.update()
+            self.clock.tick(30)
+
+    def refreshScreen(self):
+        self.window.surface.fill(Window.COLORS["black"])
 
     def drawSnake(self):
         surface = self.window.surface
         color = self.snake.color
-        initial_pos = self.snake_initial_pos
+        pos = (self.snake.pos_x, self.snake.pos_y)
         size = self.snake.size
 
-        pygame.draw.rect(surface, color, [*initial_pos, *size])
+        print(surface, color, pos, size)
+
+        pygame.draw.rect(surface, color, [*pos, *size])
+
+
+    def listen_to_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.handle_quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.snake.change_direction(0, -self.snake_vel)
+                elif event.key == pygame.K_DOWN:
+                    self.snake.change_direction(0, self.snake_vel)
+                elif event.key == pygame.K_RIGHT:
+                    self.snake.change_direction(self.snake_vel, 0)
+                elif event.key == pygame.K_LEFT:
+                    self.snake.change_direction(-self.snake_vel, 0)
+
+
+    def handle_quit(self):
+        self.window.close()
--- pysnakes/classes/Snake.py
+++ pysnakes/classes/Snake.py
@@ -2,4 +2,14 @@ class Snake:
     def __init__(self, color, position):
         self.color = color
         self.size = (10, 10)
-        self.position = position
+        (self.pos_x, self.pos_y) = position
+        self.x_direction = 0
+        self.y_direction = 0
+
+    def change_direction(self, x, y):
+        self.x_direction = x
+        self.y_direction = y
+
+    def move(self):
+        self.pos_x += self.x_direction
+        self.pos_y += self.y_direction
