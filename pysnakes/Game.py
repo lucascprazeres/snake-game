49d6e42ccca25e0a0ba7a5bd49e0d4eaa45f9ecd
lucascprazeres <olucascostadp@gmail.com>
2020-05-25 11:08:57 -0300

    window closes when the snake hits its boundries

--- pysnakes/Game.py
+++ pysnakes/Game.py
@@ -9,20 +9,39 @@ class Game:
         self.clock = pygame.time.Clock()
         self.snake = None
         # Game Rules
+        self.screen_size = (400, 300)
+        self.boundries = {
+            "x":self.screen_size[0] - 15,
+            "y":self.screen_size[1] - 15
+        }
         self.snake_initial_pos = (200, 150)
         self.snake_vel = 5
 
     def start(self):
-        self.window = Window.Window((400, 300))
+        self.window = Window.Window(self.screen_size)
         self.snake = Snake(Window.COLORS["green"], self.snake_initial_pos)
 
         while not self.game_over:
+            snake_hit_the_boundries = False
+
+            if self.boundries["x"] < self.snake.pos_x or self.snake.pos_x < 5:
+                snake_hit_the_boundries = True
+            if self.boundries["y"] < self.snake.pos_y or self.snake.pos_y < 5:
+                snake_hit_the_boundries = True
+
             self.listen_to_events()
             self.snake.move()
             self.refreshScreen()
             self.drawSnake()
             pygame.display.update()
             self.clock.tick(30)
+
+            print(snake_hit_the_boundries)
+
+            if snake_hit_the_boundries:
+                self.game_over = True
+
+        self.quit()
 
     def refreshScreen(self):
         self.window.surface.fill(Window.COLORS["black"])
@@ -33,15 +52,13 @@ class Game:
         pos = (self.snake.pos_x, self.snake.pos_y)
         size = self.snake.size
 
-        print(surface, color, pos, size)
-
         pygame.draw.rect(surface, color, [*pos, *size])
 
 
     def listen_to_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.handle_quit()
+                self.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     self.snake.change_direction(0, -self.snake_vel)
@@ -53,5 +70,5 @@ class Game:
                     self.snake.change_direction(-self.snake_vel, 0)
 
 
-    def handle_quit(self):
+    def quit(self):
         self.window.close()
